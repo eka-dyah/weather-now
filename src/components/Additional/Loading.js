d2b89af7060a320937c05b4c92c1f70487d46d8e
eka-dyah <eka.dya1205@gmail.com>
2020-09-07 16:54:19 +0700

    add loading components

---
+++ src/components/Additional/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Spinner, Row, Col } from "reactstrap";
+
+const Loading = ({ isLoading }) => (
+	<Row style={{ display: isLoading }}>
+		<Col className="text-center">
+			<Spinner />
+		</Col>
+	</Row>
+);
+
+export default Loading;
