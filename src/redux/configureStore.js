e188de372a18a79b43b3d1aebecb555af6054f9e
eka-dyah <eka.dya1205@gmail.com>
2020-09-18 16:59:51 +0700

    configure redux logger for production

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,12 +5,15 @@ import { createLogger } from "redux-logger";
 import { locationReducer } from "./reducers/location";
 import { weatherReducer } from "./reducers/weather";
 
-const composeEnhancers =
-	process.env.NODE_ENV === "development"
-		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		: null || compose;
-
-const loggerMiddleware = createLogger();
+let composeEnhancers;
+let loggerMiddleware;
+if (process.env.NODE_ENV === "development") {
+	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+	loggerMiddleware = createLogger();
+} else {
+	composeEnhancers = null || compose;
+	loggerMiddleware = null;
+}
 
 const rootReducer = combineReducers({
 	location: locationReducer,
